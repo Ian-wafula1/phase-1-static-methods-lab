557f0c94ba5661318e48592250f0254ee8167508
Ian Wafula <neocorpse@proton.me>
2025-03-28 15:50:08 +0300

    Add static methods to Formatter class

--- index.js
+++ index.js
@@ -1,3 +1,16 @@
 class Formatter {
   //add static methods here
+  static capitalize(str) {
+    return str.at(0).toUpperCase() + str.slice(1)
+  }
+  static sanitize(str) {
+    return str.replaceAll(/[^a-zA-Z0-9\s'-]+/g, '')
+  }
+  static titleize(str) {
+    return Formatter.capitalize(str.split(' ').map(word => {
+      let skip = ['a', 'an', 'but', 'of', 'and', 'for', 'at', 'by', 'from', 'the']
+      if (skip.includes(word)) return word
+      else return Formatter.capitalize(word)
+    }).join(' '))
+  }
 }
